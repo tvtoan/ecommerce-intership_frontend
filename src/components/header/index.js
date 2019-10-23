a476f7401a3379a61b2b12a9f193d121f5c2c7c5
tvtoan <14130349@st.hcmuaf.edu.vn>
2019-10-23 14:09:15 +0700

    update ui header v1

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,7 +10,7 @@ export default class index extends Component {
   render() {
     return (
       <header className="container-fluid">
-        <div className="header__top container">
+        <div className="container header__top">
           <div className="header__top--left">
             <form action="" method="post" className="header__search">
               <input
@@ -25,11 +25,26 @@ export default class index extends Component {
             <Logo />
           </div>
           <div className="header__top--right">
-            <img
-              src="https://www.alvinailey.org/sites/default/files/melanie-person.jpg"
-              alt="person-avatar"
-              className="avatar-user"
-            />
+            <div className="auth">
+              {/* <button className="flat-button auth__register">Register</button>
+              <button className="flat-button auth__login">Log In</button> */}
+              <div className="avatar-user">
+                <img
+                  src="https://www.alvinailey.org/sites/default/files/melanie-person.jpg"
+                  alt="person-avatar"
+                />
+                <div className="avatar-user__dropdown">
+                  <ul>
+                    <li>
+                      <button className="flat-button">Account setting</button>
+                    </li>
+                    <li>
+                      <button className="flat-button">Logout</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
             <div className="cart">
               <Cart />
               <span>7</span>
@@ -39,34 +54,68 @@ export default class index extends Component {
         <div className="break--style1" />
         <div className="header__bottom">
           <div className="container">
-            <nav className="nav--vertical">
-              <ul className="nav--vertical__container">
-                <li className="nav--vertical__item">
+            <nav className="nav--horizontal">
+              <ul className="nav--horizontal__container">
+                <li className="nav--horizontal__item">
                   <a href="#/">Men</a>
                   <FontAwesomeIcon
                     icon={["fas", "chevron-down"]}
-                    className="nav--vertical__item__icon"
+                    className="nav--horizontal__item__icon"
                   />
+                  <ul className="nav--horizontal__submenu">
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Tops</a>
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Bottoms</a>
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Dresses</a>
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Jackets</a>
+                      {/* <ul className="nav--horizontal__submenu">
+                        <li className="nav--horizontal__item"><a href="#/">Tops</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Bottoms</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Dresses</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Jackets</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Shoes</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Accesories</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Sale</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Accesories 1</a></li>
+                        <li className="nav--horizontal__item"><a href="#/">Sale 1</a></li>
+                      </ul> */}
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Shoes</a>
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Accesories</a>
+                    </li>
+                    <li className="nav--horizontal__item">
+                      <a href="#/">Sale</a>
+                    </li>
+                  </ul>
                 </li>
-                <li className="nav--vertical__item">
+                <li className="nav--horizontal__item">
                   <a href="#/">Ladies</a>
                   <FontAwesomeIcon
                     icon={["fas", "chevron-down"]}
-                    className="nav--vertical__item__icon"
+                    className="nav--horizontal__item__icon"
                   />
                 </li>
-                <li className="nav--vertical__item">
+                <li className="nav--horizontal__item">
                   <a href="#/">Girls</a>
                   <FontAwesomeIcon
                     icon={["fas", "chevron-down"]}
-                    className="nav--vertical__item__icon"
+                    className="nav--horizontal__item__icon"
                   />
                 </li>
-                <li className="nav--vertical__item">
+                <li className="nav--horizontal__item">
                   <a href="#/">Boys</a>
                   <FontAwesomeIcon
                     icon={["fas", "chevron-down"]}
-                    className="nav--vertical__item__icon"
+                    className="nav--horizontal__item__icon"
                   />
                 </li>
               </ul>

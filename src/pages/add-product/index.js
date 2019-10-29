f6f862693a5bc11762aabda4f3d5caae10f87655
tvtoan <14130349@st.hcmuaf.edu.vn>
2019-10-29 15:40:09 +0700

    connect upload multiple file

--- src/pages/add-product/index.js
+++ src/pages/add-product/index.js
@@ -1,13 +1,63 @@
 import React from "react";
 import { Formik } from "formik";
-import _ from 'lodash/lang';
+import _ from "lodash/lang";
 import "./style.scss";
 
 export default function AddProduct() {
   return (
     <div>
       <Formik
-        initialValues={{ image: null, email: '' }}
+        initialValues={{ image: null, email: "" }}
+        onSubmit={values => {
+          console.log("values:", values.image);
+          const cloneValue = _.cloneDeep(values);
+          delete cloneValue.image;
+          let data = new FormData();
+          data.append("images", Array.from(values.image));
+          for (let [key, value] of Object.entries(cloneValue)) {
+            data.append(key, value);
+          }
+          fetch("http://localhost:8080/upload-multi", {
+            method: "POST",
+            body: data
+          })
+            .then(res => {
+              console.log(res);
+            })
+            .catch(err => console.log(err));
+        }}
+      >
+        {({
+          values,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          setFieldValue
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            <input
+              id="file"
+              name="image"
+              type="file"
+              multiple
+              onChange={event => {
+                setFieldValue("image", event.currentTarget.files);
+              }}
+              className="form-control"
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
+      {/* <Formik
+        initialValues={{ image: null, email: "" }}
         onSubmit={values => {
           console.log("values:", values);
           const cloneValue = _.cloneDeep(values);
@@ -20,14 +70,28 @@ export default function AddProduct() {
           fetch("http://localhost:8080/upload-single", {
             method: "POST",
             body: data
-          }).then(res => {
-            console.log(res);
-          }).catch(err => console.log(err));
+          })
+            .then(res => {
+              console.log(res);
+            })
+            .catch(err => console.log(err));
         }}
       >
-        {({ values, handleSubmit, handleChange, handleBlur, setFieldValue }) => (
+        {({
+          values,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          setFieldValue
+        }) => (
           <form onSubmit={handleSubmit}>
-            <input name="email" type="email" onChange={handleChange} onBlur={handleBlur} value={values.email}/>
+            <input
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
             <input
               id="file"
               name="image"
@@ -40,7 +104,7 @@ export default function AddProduct() {
             <button type="submit">Submit</button>
           </form>
         )}
-      </Formik>
+      </Formik> */}
     </div>
   );
 }

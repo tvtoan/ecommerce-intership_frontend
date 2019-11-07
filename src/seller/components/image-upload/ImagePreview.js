bb1470443112c98a0394b26dca5f17c2c12b807a
tvtoan <14130349@st.hcmuaf.edu.vn>
2019-11-07 10:08:59 +0700

    convert image to base64

--- src/seller/components/image-upload/ImagePreview.js
+++ src/seller/components/image-upload/ImagePreview.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // 3rd packages
 import PropTypes from "prop-types";
 // static resources
 import { ReactComponent as CloseIcon } from "assets/images/seller/icons/close-1.svg";
 
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+    reader.readAsDataURL(file);
+  });
+}
+
 export default function ImagePreview(props) {
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+
+  const handlePreview = async (file) => {
+    let base64 = await getBase64(file);
+    setPreviewVisible(true);
+    setPreviewImage(base64);
+  };
+
+  useEffect(() => {
+    console.log("props:", props.dataFile);
+    handlePreview(props.dataFile);
+  })
+
   return (
     <div className="image-upload__preview">
-      <img src={props.image} alt="file-name" />
+      <img src={previewImage} alt="file-name" />
       <span className="image-upload__preview-remove">
         <CloseIcon className="icons" />
       </span>
@@ -16,5 +39,5 @@ export default function ImagePreview(props) {
 }
 
 ImagePreview.propTypes = {
-  image: PropTypes.string
+  dataFile: PropTypes.object
 };
--- src/seller/components/image-upload/ImageUpload.js
+++ src/seller/components/image-upload/ImageUpload.js
@@ -1,48 +1,25 @@
 // core
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // 3rd packages
 import PropTypes from "prop-types";
 // components
 import ImagePreview from "./ImagePreview";
 // static resources
-import productCover from "assets/images/seller/product-1-cover.jpeg";
-import productImage1 from "assets/images/seller/product-1-image.jpeg";
+// import productCover from "assets/images/seller/product-1-cover.jpeg";
+// import productImage1 from "assets/images/seller/product-1-image.jpeg";
 import { ReactComponent as AddIcon } from "assets/images/seller/icons/add.svg";
 
 const imageBase64 =
   "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
 
-const toBase64 = file =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
-
-const imageToBase64 = file => {
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  return reader.result;
-}
-
 export default function ImageUpload(props) {
-  useEffect(() => {
-    console.log("Files:", props.droppedFiles);
-    for (let file of props.droppedFiles) {
-      // toBase64(file).then(result => {
-      //   console.log("Base64:", result);
-      // }).catch(err => console.log("Error:", err));
-      console.log("Base64:", imageToBase64(file));
-    }
-  })
 
   return (
     <div className="image-upload-wrapper">
       <div className="image-upload__list">
         {props.droppedFiles.map(file => (
           <div key={file.name} className="image-upload__list-item">
-            <ImagePreview image={imageToBase64(file)} />
+            <ImagePreview dataFile={file} />
           </div>
         ))}
         <div className="image-upload__list-item">

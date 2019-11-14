cfca5a1ccec5c29971ea16da1e1d74171e5b90b1
tvtoan <14130349@st.hcmuaf.edu.vn>
2019-11-14 21:14:43 +0700

    fix bug style CategoryPage

--- src/seller/routes/AuthRoute.js
+++ src/seller/routes/AuthRoute.js
@@ -1,12 +1,12 @@
 import React from "react";
 // 3rd packages
-import { Route, Redirect, useRouteMatch } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 // layouts
 import AuthLayout from "../layouts/AuthLayout";
 
 export default function AuthRoute({ component: Component, ...props }) {
-  let match = useRouteMatch();
-  console.log("isAuthenticated:", props.isAuthenticated);
+  // let match = useRouteMatch();
+  // console.log("isAuthenticated:", props.isAuthenticated);
   return (
     <Route
       {...props}
